91f3470c23a5832863fddaeb336c78cef5522bd0
wtnbyk-a19 <theskyistehlimit.0819@gmail.com>
2022-06-12 20:26:02 +0900

    feat: Userの初期化処理を作成

--- src/Domain/User/User.go
+++ src/Domain/User/User.go
@@ -1,8 +1,16 @@
 package User
 
+// User TODO: 値オブジェクトの作成
 type User struct {
 	Id       int
 	Name     string
 	Email    string
 	Password string
+}
+
+func NewUser(id int, name string, email string, password string) (*User, error) {
+	// User TODO: ガード節の作成
+	return &User{
+		id, name, email, password,
+	}, nil
 }
